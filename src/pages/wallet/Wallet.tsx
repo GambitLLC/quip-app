371cefd831262f392bffa8085e83bd9833f359dd
bendgk <benkosten@gmail.com>
2023-08-28 19:14:11 -0400

    + suspense maybe???

--- src/pages/wallet/Wallet.tsx
+++ src/pages/wallet/Wallet.tsx
@@ -2,13 +2,16 @@ import {p, Screen, spacing, TopWalletInfo, TransactionHistory} from "@/lib";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import {ParamListBase} from "@react-navigation/native";
 import {StyleSheet, View} from "react-native";
+import {Suspense} from "react";
 
 export default function Wallet({route, navigation}: NativeStackScreenProps<ParamListBase, "wallet">) {
   return (
     <Screen style={[spacing.fill]}>
       <View style={[spacing.fill, p('a', 4)]}>
         <TopWalletInfo/>
-        <TransactionHistory/>
+        <Suspense>
+          <TransactionHistory/>
+        </Suspense>
       </View>
     </Screen>
   )

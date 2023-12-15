0a92d33469ea25aa3bbfe7dbcb10cad9454fbb78
bendgk <benkosten@gmail.com>
2023-12-15 11:01:50 -0500

    + add unity post-game integration

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -4,11 +4,16 @@ import {ButtonClick, flex, Screen, spacing, Text, typography} from "@/lib";
 import theme from "@/util/Theme";
 import UnityView from '@azesmway/react-native-unity';
 
-function Unity() {
+function Unity({route, navigation}: GameProps) {
   return (
     <View style={{ flex: 1, width: "100%", height: "100%" }}>
       <UnityView onUnityMessage={(event) => {
         console.log(`UnityMessage: ${event.nativeEvent.message}`)
+        const message = event.nativeEvent.message
+
+        if (message === "postGame") {
+          navigation.push("postGame")
+        }
       }} style={{ flex: 1 }}/>
     </View>
   )
@@ -17,7 +22,7 @@ function Unity() {
 export function Game({route, navigation}: GameProps) {
   return (
     <Screen hasStatusBar={false} hasSafeArea={false} style={[spacing.fill]}>
-      <Unity/>
+      <Unity route={route} navigation={navigation}/>
     </Screen>
   )
 }
